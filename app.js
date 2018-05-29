65196f8b5b46687974da694bb9c26ae5923e7ae4
Tales Medeiros <t4les313@gmail.com>
2018-05-28 21:57:30 -0300

    added scroll loading

--- app.js
+++ app.js
@@ -3,30 +3,25 @@ let section = document.querySelector('section')
 
 /*******************************************************************************/
 
+let i=0 //usado para percorrer o array de fotos da Terra
+let array //array (de json) das fotas da Terra
+let d //objeto que vai guardar algumas informações sobre data e hora de cada foto
 let apiKey = '7BkyIyS2p0ChLQBSiTGSlbz7b1n4EzDBzFoQH8Xb'
+let imgLink = 'https://epic.gsfc.nasa.gov/archive/natural/'
 let yesterdayEarth = `https://api.nasa.gov/EPIC/api/natural?api_key=${apiKey}`
 let apod = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
 // apod =  Astronomy Picture of the Day
 
 /*******************************************************************************/
 
-function preparePage() {
-    fetch(yesterdayEarth).then(r => r.json()).then(array => {
-        let d = getDateInfos(array)
-        h1Day.insertAdjacentText('beforeend', "Fotos do dia "+d.day+"/"+d.month+"/"+d.year)
-        let str = ""
-        let i = 0
-        let link = 'https://epic.gsfc.nasa.gov/archive/natural/'
-        for(let item of array)
-            str += `
-                <div class="bloco">
-                    <a href="${link}${d.year}/${d.month}/${d.day}/png/${item.image}.png" target="blank" rel="noopener">
-                    <img src="${link}${d.year}/${d.month}/${d.day}/png/${item.image}.png">
-                    </a>
-                    <span>${d.hour[i++]}</span>
-                </div>`
-        section.insertAdjacentHTML('beforeend', str)
-    })
+async function preparePage() {
+
+    let response = await fetch(yesterdayEarth)
+    array = await response.json()
+
+    d = getDateInfos(array)
+    h1Day.insertAdjacentText('beforeend', "Fotos do dia "+d.day+"/"+d.month+"/"+d.year)
+    addBlocos(9)
 }
 
 function getDateInfos(array) {
@@ -37,6 +32,36 @@ function getDateInfos(array) {
     obj.day = array[0].date.substr(8, 2)
     obj.hour = array.map(i => {return i.date.substr(11, 8)})
     return obj
+}
+
+window.addEventListener('scroll', function() {
+    let at = document.body.offsetHeight // at = alturaTotal do documento
+    let y = window.scrollY // y = pixels scrollados desde o inicio do documento
+    let altura = window.innerHeight // 648 no monitor de casa
+    sobra = at-altura
+    if(y == sobra)
+        addBlocos(3)
+})
+
+function addBlocos(qtd) {
+    let str = ""
+    let max = i+qtd
+    for(; i<max; i++) {
+        if(array[i] == undefined)
+            break
+        str += getProxBloco()
+    }
+    section.insertAdjacentHTML('beforeend', str)
+}
+
+function getProxBloco() {
+    return `
+    <div class="bloco">
+        <a href="${imgLink}${d.year}/${d.month}/${d.day}/png/${array[i].image}.png" target="blank" rel="noopener">
+            <img src="${imgLink}${d.year}/${d.month}/${d.day}/png/${array[i].image}.png">
+        </a>
+        <span>${d.hour[i]}</span>
+    </div>`
 }
 
 Window.onload = preparePage()
