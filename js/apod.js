e8ad095ce84ba4ba0f2ae4163289231cf44fb45e
Tales Medeiros <t4les313@gmail.com>
2018-08-24 23:07:13 -0300

    date option added on apod page

--- js/apod.js
+++ js/apod.js
@@ -1,33 +1,59 @@
-let title = document.querySelector('h1#title')
+let intro_text = document.querySelector('p#intro')
+let button = document.querySelector('input#date-submit')
+let error_msg = document.querySelector('p#error-msg')
+let apod_title = document.querySelector('h2#apod-title')
+let media_date = document.querySelector('p#media-date')
 let media = document.querySelector('div#media')
 let explanation = document.querySelector('p#explanation')
 
 /*******************************************************************************/
 
-let json
+let json //will receive the apod json
 let apiKey = '7BkyIyS2p0ChLQBSiTGSlbz7b1n4EzDBzFoQH8Xb'
-let apod = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+let apodLink = 'https://api.nasa.gov/planetary/apod'
+let todayApod = `${apodLink}?api_key=${apiKey}`
 
-// let demo = 'https://api.nasa.gov/planetary/apod?date=2018-06-18&api_key=DEMO_KEY'
+async function main() {
 
-async function preparePage() {
+    await loadAPOD(todayApod) //loading the today's apod
 
-    let response = await fetch(apod)
-    // let response = await fetch(demo)
+    intro_text.textContent = `Today is ${json.date}, but you can choose a different date
+        right below!`
+
+    button.addEventListener('click', async function(e) {
+        e.preventDefault()
+        let queryDate = document.querySelector('input#date-input').value
+        let link = `${apodLink}?date=${queryDate}&api_key=${apiKey}`
+        loadAPOD(link)
+    })
+
+}
+
+async function loadAPOD(link) {
+
+    let response = await fetch(link)
     json = await response.json()
-    console.log(json)
 
-    title.textContent = json.title
+    if(json.code == 400) { //invalid date
+        error_msg.textContent = "Invalid date!"
+        return
+    }else
+        error_msg.textContent = ""
 
+    apod_title.textContent = json.title
+    media_date.textContent = json.date
+
+    media.innerHTML = ''
+    media.insertAdjacentHTML('beforeend', `<p id="media-date">${json.date}</p>`)
     if(json.media_type == 'image')
-        media.insertAdjacentHTML('afterbegin', `
+        media.insertAdjacentHTML('beforeend', `
             <a href="${json.hdurl}" target="_blank"><img src="${json.url}"></a>`)
     else if(json.media_type == 'video')
-        media.insertAdjacentHTML('afterbegin', `
+        media.insertAdjacentHTML('beforeend', `
             <iframe src="${json.url}" allowfullscreen>`)
 
     explanation.textContent = json.explanation
 
 }
 
-preparePage()
+main()
